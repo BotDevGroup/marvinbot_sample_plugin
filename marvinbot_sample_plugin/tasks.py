09e8fad34b542fe4ae4cf366af81a5592b0e8a85
Ricardo Restituyo <ricardo@codemera.com>
2016-09-30 23:46:07 -0400

    Add sample tasks (previously on core)

--- marvinbot_sample_plugin/tasks.py
+++ marvinbot_sample_plugin/tasks.py
@@ -1,6 +1,6 @@
 from marvinbot.core import get_adapter
 from marvinbot.utils import get_message
-from marvinbot.handlers import CommandHandler
+from marvinbot.handlers import Filters, CommandHandler, MessageHandler
 from celery.utils.log import get_task_logger
 from celery import task
 from marvinbot_sample_plugin.models import WitnessedUser
@@ -25,4 +25,31 @@ def witness_command(update, *args):
     else:
         adapter.bot.sendMessage(chat_id=update.message.chat_id, text="Mediocre! (already existed)")
 
+
+@task()
+def start_command(update, *args):
+    log.info('Start command caught')
+    adapter.bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!: {}".format(args))
+
+
+@task()
+def bowdown(update, *args):
+    update.message.reply_text('Yes, master **bows**')
+
+
+@task()
+def gaze_at_pic(update):
+    update.message.reply_text('Nice pic, bro')
+
+
+@task()
+def salutation_initiative(update):
+    update.message.reply_text("'zup")
+
+
 adapter.add_handler(CommandHandler('witness', witness_command, call_async=True))
+adapter.add_handler(CommandHandler('start', start_command, call_async=True))
+adapter.add_handler(MessageHandler(Filters.photo, gaze_at_pic))
+adapter.add_handler(MessageHandler([Filters.text, lambda msg: msg.text.lower() in ['hola', 'hi', 'klk', 'hey']],
+                                   salutation_initiative,
+                                   strict=True))
