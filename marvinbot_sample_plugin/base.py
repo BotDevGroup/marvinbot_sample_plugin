e68aa676fb5f04c01c5935b511c294939186c32d
Ricardo Restituyo <ricardo@codemera.com>
2016-11-05 17:54:47 -0400

    Add CallbackQueryHandler example

--- marvinbot_sample_plugin/base.py
+++ marvinbot_sample_plugin/base.py
@@ -1,10 +1,11 @@
 from marvinbot_sample_plugin.tasks import (
     witness_command, start_command, gaze_at_pic, salutation_initiative
 )
-from marvinbot.handlers import CommonFilters, CommandHandler, MessageHandler
+from marvinbot.handlers import CommonFilters, CommandHandler, MessageHandler, CallbackQueryHandler
 from marvinbot.signals import plugin_reload
 from marvinbot.plugins import Plugin
-from marvinbot.utils import localized_date
+from marvinbot.utils import localized_date, get_message
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from datetime import timedelta
 
 import logging
@@ -44,6 +45,11 @@ class SamplePlugin(Plugin):
                          .add_argument('--foo', help='foo help')
                          .add_argument('--bar', help='bar help'), 0)
 
+        self.add_handler(CommandHandler('test_callback', self.send_callback,
+                                        command_description="Sends a button to test CallbackQueryHandler"))
+        self.add_handler(CallbackQueryHandler("{}:{}".format(self.name, 'test-callback'),
+                                              self.handle_callback))
+
     def setup_schedules(self, adapter):
         # See: https://apscheduler.readthedocs.io/en/latest/modules/schedulers/base.html#apscheduler.schedulers.base.BaseScheduler.add_job
         # Give the job an explicit ID and tell it to replace existing, this avoids
@@ -65,6 +71,22 @@ class SamplePlugin(Plugin):
         log.info("Responding to you_there: args: %s, kwargs: %s", str(args), str(kwargs))
         update.message.reply_text('Me here 👀, are _you_? args:"{}", kwargs: "{}"'.format(str(args), str(kwargs)),
                                   parse_mode='Markdown')
+
+    def send_callback(self, update, *args, **kwargs):
+        message = get_message(update)
+        callback_data = "{name}:{action}:{message_id}".format(name=self.name, action="test-callback",
+                                                              message_id=message.message_id)
+        custom_keyboard = [[InlineKeyboardButton("Click me", callback_data=callback_data), ]]
+        reply_markup = InlineKeyboardMarkup(custom_keyboard)
+        self.adapter.bot.sendMessage(chat_id=message.chat_id,
+                                     text="Here are some buttons.",
+                                     reply_markup=reply_markup)
+
+    def handle_callback(self, update, *args, **kwargs):
+        query = update.callback_query
+        self.adapter.bot.editMessageText(text="Selected option: %s" % query.data,
+                                         chat_id=query.message.chat_id,
+                                         message_id=query.message.message_id)
 
 
 def my_scheduled_func():
